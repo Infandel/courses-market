2ae2d75242b05c6cf75027a853a98fa22d4d0988
Infandel <tomahawk92@gmail.com>
2025-08-31 17:52:17 +0500

    Added svgr/webpack to webpack config aswell.

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,20 @@ const nextConfig: NextConfig = {
 			},
 		},
 	},
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: [
+				{
+					loader: '@svgr/webpack',
+					options: {
+						icon: true,
+					},
+				},
+			],
+		});
+		return config;
+	},
 };
 
 export default nextConfig;
