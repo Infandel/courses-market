24de681eea96c91b78d9193ebed9fe9001302b77
Infandel <tomahawk92@gmail.com>
2025-09-25 04:42:02 +0500

    Added small animation on buttons. Started a11y fixing with lighthouse.

--- shared/ui/button/Button.tsx
+++ shared/ui/button/Button.tsx
@@ -2,18 +2,20 @@ import { ButtonProps } from './Button.props';
 import ArrowIcon from '@/public/assets/arrow.svg';
 import styles from './Button.module.css';
 import clsx from 'clsx';
+import { motion } from 'framer-motion';
 
 export const Button = ({ appearance, arrow = 'none', children, className, ...props }: ButtonProps) => {
 	return (
-		<button
+		<motion.button
 			className={clsx(styles.button, className, {
 				[styles.primary]: appearance === 'primary',
 				[styles.ghost]: appearance === 'ghost',
 			})}
+			whileHover={{ scale: 1.05 }}
 			{...props}
 		>
 			{children}
 			{arrow !== 'none' && <ArrowIcon className={clsx(styles.arrow, { [styles.down]: arrow === 'down' })} />}
-		</button>
+		</motion.button>
 	);
 };
